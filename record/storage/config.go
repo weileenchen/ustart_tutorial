c3bc2fe05c04103a669893fa03537223c61526b1
weileenchen <weileen.chen@nyu.edu>
2019-06-05 23:12:37 -0400

    uncommented

--- record/storage/config.go
+++ record/storage/config.go
@@ -1,16 +1,16 @@
 package storage
 
-// // import (
-// // 	"github.com/sea350/ustart_tutorial/record/storage/elastic"
-// // )
+// import (
+// 	"github.com/sea350/ustart_tutorial/record/storage/elastic"
+// )
 
-// //determines the runtime behavior of the ElasticSearch backed server
-// type Config struct {
-// 	useDummy      bool
-// 	ElasticConfig *elasticstore.Config
-// }
+//determines the runtime behavior of the ElasticSearch backed server
+type Config struct {
+	useDummy      bool
+	ElasticConfig *elasticstore.Config
+}
 
-// //ESConfig -- returns default config object
-// func ESNewConfig() *Config {
-// 	return &Config{ElasticConfig: elasticstore.NewConfig()}
-// }
+//ESConfig -- returns default config object
+func ESNewConfig() *Config {
+	return &Config{ElasticConfig: elasticstore.NewConfig()}
+}
