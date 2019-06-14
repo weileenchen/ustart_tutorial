a95f2964207ee7a06fe523bc94fc7ed164a1a8a8
weileenchen <weileen.chen@nyu.edu>
2019-06-14 14:18:01 -0400

    Thirteenth Commit

--- record/storage/storage.go
+++ record/storage/storage.go
@@ -8,8 +8,11 @@ import (
 
 //Storage : interface for data
 type Storage interface {
+	//NewRequest done in SQL -- submit a new row with the data
 	New(context.Context, string, string, string, string) error
+	//HistoryRequest pulls users rental history
 	History(context.Context, string) (recordpb.Record, error)
+	//PayRequest looks for most recent unpaid history entry; pays it off with that quantity
 	//fix later, figure out how to implement Pay
 	Pay(context.Context, []string, bool, map[string][]string, string) ([]string, error)
 
