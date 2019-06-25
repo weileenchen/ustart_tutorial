65d82237ce76ddc4859cf087478ccb4e666c74b1
weileenchen <weileen.chen@nyu.edu>
2019-06-25 01:15:27 -0400

    API work

--- api/rest/car/cmd/runMicroService_sample.go
+++ api/rest/car/cmd/runMicroService_sample.go
@@ -39,7 +39,48 @@ func main() {
 
 	//Assigning the handler functions to a url
 	http.HandleFunc("/", nil)
-	http.HandleFunc("/pull", restAPI.Pull)
+	package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	_ "github.com/lib/pq"
+	"github.com/weileenchen/ustart_tutorial/api/rest/car"
+	//prof "github.com/sea350/ustart_tutorial/customer"
+	//"github.com/sea350/ustart_tutorial/customer/storage"
+	//elasticstore "github.com/sea350/ustart_tutorial/customer/storage/elastic"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Dialing up...")
+
+	var config customer.Config
+
+	//Importing configuration from json
+	file, err := os.Open("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	//Generating api object
+	restAPI, err := customer.New(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	//Assigning the handler functions to a url
+	http.HandleFunc("/", nil)
+	http.HandleFunc("/toggleAvailable", restAPI.toggleAvailabke)
 	http.HandleFunc("/register", restAPI.Register)
 	http.HandleFunc("/search", nil) //Not yet implemented for REST
 
